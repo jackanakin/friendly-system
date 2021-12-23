c6116247d0563abbc17af2c45c72c82d1d494451
Jardel Kuhn <jardelkuhn@gmail.com>
2021-12-23 08:54:34 -0300

    chore: internal error handler

---
+++ backend/src/app/@exceptions/_ExceptionDefault.ts
@@ -0,0 +1,6 @@
+export default {
+    internal: {
+        message: "Erro interno, tente novamente ou contate o administrador",
+        code: 500,
+    },
+}
---
+++ backend/src/app/@exceptions/_handler/InternalErrorHandler.ts
@@ -0,0 +1,11 @@
+import chalk from "chalk";
+import { Response } from "express";
+import _ExceptionDefault from "../_ExceptionDefault";
+
+export function internalErrorHandler(error: any, res: Response): Response {
+    console.log(chalk.bold.red(`${error}`));
+
+    return res
+        .status(_ExceptionDefault.internal.code)
+        .json({ error: _ExceptionDefault.internal.message });
+}
